f15eb4582be9af73a4a67a4e3fcbcf535a84ca8e
Yami <Sy168888>
2020-03-16 00:33:43 +0800

    完成扫一扫、地图调用、获取地址信息、获取网络信息、微信支付、DEMO、文档

--- WechatJSSDK.js
+++ WechatJSSDK.js
@@ -1,9 +1,7 @@
-(function () {
+;(function () {
+    'use strict';
 
-
-    // TODO weixinJSBridgeReady https://www.jianshu.com/p/d7f5f5131783
-
-    function WECHAT_JSSDK(params) {
+    function WechatJSSDK(params) {
         let _this = this;
 
         //合并指定配置到默认配置,
@@ -21,8 +19,7 @@
             desc: '',
             link: location.href,
             imgUrl: this._is_undefined(document.images[0]) ? '' : document.images[0].src,
-            complete: function () {
-            },
+            complete: function () {},
         };
 
         this._log('默认分享', this.share_default_data);
@@ -30,7 +27,7 @@
 
         this.envpass = false;
 
-        if (!this._check_wechat_app()) {
+        if (!this._check_in_wechat_app()) {
             this._log('环境', '非微信内');
             return this;
         }
@@ -48,22 +45,25 @@
                     wx.ready(function () {
                         _this.envpass = true;
                         _this.share();
-                    });
-                    wx.error(function () {
 
+                        return _this;
+                    });
+
+                    wx.error(function (error) {
+                        this._log('签名校验失败', error)
+                        return this;
                     });
                 }
             })
-            .catch(error => this._log('请求签名接口发生失败', error));
-
-
-        return this;
+            .catch(error => {
+                this._log('请求签名接口发生失败', error)
+                return this;
+            });
     }
 
 
     // 分享
-    WECHAT_JSSDK.prototype.share = function (params) {
-
+    WechatJSSDK.prototype.share = function (params) {
         if (!this.envpass) {
             return this;
         }
@@ -72,11 +72,10 @@
 
         wx.updateAppMessageShareData(share_data);
         wx.updateTimelineShareData(share_data);
-
     };
 
     // 隐藏菜单
-    WECHAT_JSSDK.prototype.hideAllNonBaseMenuItem = function (params) {
+    WechatJSSDK.prototype.hideAllNonBaseMenuItem = function () {
 
         if (!this.envpass) {
             return this;
@@ -86,7 +85,7 @@
     };
 
     // 显示菜单
-    WECHAT_JSSDK.prototype.showAllNonBaseMenuItem = function (params) {
+    WechatJSSDK.prototype.showAllNonBaseMenuItem = function () {
 
         if (!this.envpass) {
             return this;
@@ -95,33 +94,92 @@
 
     };
 
+    // 调用扫一扫
+    WechatJSSDK.prototype.scan = function (params) {
+
+        if (!this.envpass) {
+            return this;
+        }
+
+        let opts = Object.assign({
+            needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+            scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+            success:function () {
+
+            }
+        }, params);
+
+        return wx.scanQRCode(opts);
+    };
+
+    // 微信支付
+    WechatJSSDK.prototype.pay = function (opts) {
+        if (!this.envpass) {
+            return this;
+        }
+        return wx.chooseWXPay(opts);
+    };
+
+    // 网络状态接口
+    WechatJSSDK.prototype.getNetworkType = function(opts) {
+
+        if (!this.envpass) {
+            return this;
+        }
+
+        wx.getNetworkType(opts);
+    };
+
+    // 在腾讯地图中打开指定位置
+    WechatJSSDK.prototype.openLocation = function(opts) {
+
+        if (!this.envpass) {
+            return this;
+        }
+
+        return wx.openLocation(opts);
+    };
+
+    // 获取用户当前地址位置信息
+    WechatJSSDK.prototype.getLocation = function(params) {
+
+        if (!this.envpass) {
+            return this;
+        }
+
+        let opts = Object.assign({
+            type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+        }, params);
+
+        return wx.getLocation(opts);
+    };
+
 
     /** 下面都是私有方法 */
-    // 检测变量是否定义
-    WECHAT_JSSDK.prototype._is_undefined = function (v) {
+    // 检测变量是否是 undefined 或 null
+    WechatJSSDK.prototype._is_undefined = function (v) {
         return v === undefined || v === null
     };
 
     // 检测是否在微信APP内
-    WECHAT_JSSDK.prototype._check_wechat_app = function () {
+    WechatJSSDK.prototype._check_in_wechat_app = function () {
         return /MicroMessenger/i.test(navigator.userAgent) ? true : false;
     };
 
     // 打印 日志
-    WECHAT_JSSDK.prototype._log = function(k, v){
+    WechatJSSDK.prototype._log = function(k, v){
         if(this.opts.debug) {
             console.log(k, v);
         }
     };
 
     // ajax 请求
-    WECHAT_JSSDK.prototype._request = function (method, url, payload) {
+    WechatJSSDK.prototype._request = function (method, url, payload) {
 
         return fetch(url, {
             body: JSON.stringify(payload),
             method: method.toUpperCase(),
             cache: 'no-cache',
-            mode: 'cors',
             headers: new Headers({
                 'Accept-Charset': 'utf-8',
                 'Content-Type': 'application/json',
@@ -132,5 +190,5 @@
             .catch(err => err.json());
     };
 
-    window.WECHAT_JSSDK = WECHAT_JSSDK;
+    window.WechatJSSDK = WechatJSSDK;
 })();
