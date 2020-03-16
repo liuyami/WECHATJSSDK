c61c490cf03f5cadd519ffb88f7754859415b2a7
Yami <evila@qq.com>
2020-03-16 18:31:59 +0800

    等待通过再自行

--- WechatJSSDK.js
+++ WechatJSSDK.js
@@ -101,6 +101,12 @@
             return this;
         }
 
+        let timer = window.setInterval(function(){
+            if(this.envpass) {
+                clearInterval(timer);
+            }
+        },100);
+
         let opts = Object.assign({
             needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
             scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
